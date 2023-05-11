82e0ee98886466334629fe0df509854dddde99aa
Utente <Utente@DESKTOP-FQSJVLU.station>
2023-05-11 21:30:13 +0200

    new class

---
+++ src/main/java/com/example/jse/m05/s03/my/KpopGroupMember.java
@@ -0,0 +1,17 @@
+package com.example.jse.m05.s03.my;
+
+public class KpopGroupMember {
+    private char gender;
+    private String memberName;
+    private int memberAge;
+    private String placeOfBirth;
+    
+    
+    public KpopGroupMember(char gender, String memberName, int memberAge, String placeOfBirth) {
+        super();
+        this.gender = gender;
+        this.memberName = memberName;
+        this.memberAge = memberAge;
+        this.placeOfBirth = placeOfBirth;
+    }
+}
--- src/main/java/com/example/jse/m05/s03/my/Main.java
+++ src/main/java/com/example/jse/m05/s03/my/Main.java
@@ -6,11 +6,15 @@ public class Main {
         System.out.println(herbie);
         String message = herbie.steer(true);
         System.out.println(message);
-        
+
 //        KpopGroup
         KpopGroup bts = new KpopGroup("BTS", "Big Hit", 10);
         System.out.println(bts);
         String message2 = bts.goOnTour(true);
         System.out.println(message2);
+
+//        KpopGroupMember
+        KpopGroupMember jin = new KpopGroupMember('m', "Kim Seokjin", 30, "South Korea");
+        System.out.println(jin);
     }
 }
